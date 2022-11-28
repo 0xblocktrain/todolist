1f012e40a99068cd8c84c03b64e6813ff21a1530
Deveshb15 <deveshb583@gmail.com>
2022-11-29 00:00:32 +0530

    feat: complete task

--- src/App.js
+++ src/App.js
@@ -55,6 +55,23 @@ function App() {
     }
   }
 
+  
+  const completeTask = async(task) => {
+    try {
+      console.log(tasks)
+      const taskIndex = tasks.findIndex(a => a[0] === task)
+      console.log("index ", taskIndex)
+      const completeTask = await contract.completeTask(taskIndex)
+      console.log(completeTask)
+      await completeTask.wait()
+      const newTasks = tasks.filter(a => a[0] !== task)
+      console.log(newTasks)
+      setTasks(newTasks)
+    } catch(err) {
+      console.log(err)
+    }
+  }
+
 	useEffect(() => {
 		if (contract) {
 			getTasks();
@@ -91,7 +108,7 @@ function App() {
                     !taskItem[1] && (
                       <div className="flex items-center justify-between gap-3 py-2">
                         <p>{taskItem[0]}</p>
-                        <button className="px-2 rounded-xl bg-white text-black border border-blue-400 transform hover:scale-105">Complete</button>
+                        <button onClick={() => completeTask(taskItem[0])} className="px-2 rounded-xl bg-white text-black border border-blue-400 transform hover:scale-105">Complete</button>
                       </div>
                     )
                   }
