3c542a8cdabc63aa67e46404559455707c690f19
Deveshb15 <deveshb583@gmail.com>
2022-11-28 23:19:27 +0530

    chore: display not completed task

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { useAccount, useContract, useSigner } from "wagmi";
 import { CONTRACT_ABI, CONTRACT_ADDRESS } from "./contract";
 
 function App() {
+	const [tasks, setTasks] = useState([]);
 
 	const { address } = useAccount();
 	const { data: signer } = useSigner();
@@ -17,19 +18,21 @@ function App() {
 
 	const getTasks = async () => {
 		try {
-      let tasks = []
+			let tasks = [];
 			let tasks_count = await contract.count();
 			console.log("TOTAL NUMBER OF TASKS ", tasks_count.toString());
-      if(tasks_count) {
-        tasks_count = (+tasks_count)
-        for(let i = 0; i < tasks_count; i++) {
-          const task = await contract.tasks(i)
-          if(task) {
-            tasks.push(task)
-          }
-        }
-      }
-      console.log(tasks)
+			if (tasks_count) {
+				tasks_count = +tasks_count;
+				// Fetch tasks
+				for (let i = 0; i < tasks_count; i++) {
+					const task = await contract.tasks(i);
+					if (task) {
+						tasks.push(task);
+					}
+				}
+			}
+			// Set task in the state
+			setTasks(tasks);
 		} catch (err) {
 			console.log(err);
 		}
@@ -53,6 +56,25 @@ function App() {
 			>
 				<h1 className="text-4xl font-extrabold">Todo List</h1>
 				{!address && <ConnectButton />}
+
+        {/* Add Task */}
+
+        {/* All Tasks */}
+				<div className="flex items-center justify-center flex-col">
+					{tasks.length > 0 &&
+						tasks.map((taskItem, i) => {
+							return (
+								<div className="flex items-center justify-between" key={i}>
+                  {/* Check if task is not completed */}
+                  {
+                    !taskItem[1] && (
+                      <p>{taskItem[0]}</p>
+                    )
+                  }
+								</div>
+							);
+						})}
+				</div>
 			</div>
 		</div>
 	);
