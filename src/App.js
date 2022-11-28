1281a78c8e99fcfc5f83b4f6828b29a8c843c213
Deveshb15 <deveshb583@gmail.com>
2022-11-28 22:35:08 +0530

    feat: rainbowkit installed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+import { useAccount } from 'wagmi'
+
 function App() {
-  return (
-    <div className="flex items-center justify-center min-h-screen bg-black text-white">
-      <h1 className='text-4xl font-extrabold'>
-        Todo List
-      </h1>
+
+  const { address } = useAccount()
+
+	return (
+    <div className="flex flex-col justify-center items-center bg-black text-white">
+      <div className="flex items-center justify-between w-full px-4 py-2">
+        <p className="text-xl font-bold">Todo-List</p>
+        {
+          address && (
+            <ConnectButton />
+          )
+        }
+      </div>
+      <div style={{ minHeight: '95vh' }} className="flex flex-col items-center justify-center gap-4 w-full">
+        <h1 className="text-4xl font-extrabold">Todo List</h1>
+        {
+          !address && (
+            <ConnectButton />
+          )
+        }
+      </div>
     </div>
-  );
+	);
 }
 
 export default App;
